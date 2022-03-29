339550252503c94e81ba36dbff65f2fa0a157528
Gaurav <gaurav.180410107121@gmail.com>
2022-03-29 08:12:34 +0300

    added database for contact form

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAG5tUuOg6twu5gg8-TrhrV0PHkH6mwBig",
+  authDomain: "portfoliodata-d7557.firebaseapp.com",
+  databaseURL: "https://portfoliodata-d7557-default-rtdb.firebaseio.com",
+  projectId: "portfoliodata-d7557",
+  storageBucket: "portfoliodata-d7557.appspot.com",
+  messagingSenderId: "914033159239",
+  appId: "1:914033159239:web:16e21a3018f00f46535562",
+};
+
+// Initialize firebase
+firebase.initializeApp(firebaseConfig);
+
 $("document").ready(function () {
   var typed = new Typed("#typing", {
     strings: ["WEB DEVELOPER.", "UI/UX DESIGNER.", "INVESTOR."],
@@ -17,3 +30,37 @@ menu.addEventListener("click", () => {
   socialMedia.classList.toggle("active");
   navbar.classList.toggle("active");
 });
+
+// reference your database
+var contactFormDB = firebase.database().ref("contactForm");
+
+document.getElementById("contactForm").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+
+  var name = getElementVal("name");
+  var mail = getElementVal("mail");
+  var phone = getElementVal("phone");
+  var msg = getElementVal("msg");
+
+  saveData(name, mail, phone, msg);
+
+  // reset form
+  document.getElementById("contactForm").reset();
+}
+
+const saveData = (name, mail, phone, msg) => {
+  var newContactForm = contactFormDB.push();
+
+  newContactForm.set({
+    name: name,
+    mail: mail,
+    phone: phone,
+    msg: msg,
+  });
+};
+
+const getElementVal = (id) => {
+  return document.getElementById(id).value;
+};
