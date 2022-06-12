2033b8cd6ee48dcdd554b894fc5e3e12cb89b3de
Gaurav <gaurav.180410107121@gmail.com>
2022-06-12 10:47:29 +0300

    contact form working

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyAG5tUuOg6twu5gg8-TrhrV0PHkH6mwBig",
-  authDomain: "portfoliodata-d7557.firebaseapp.com",
-  databaseURL: "https://portfoliodata-d7557-default-rtdb.firebaseio.com",
-  projectId: "portfoliodata-d7557",
-  storageBucket: "portfoliodata-d7557.appspot.com",
-  messagingSenderId: "914033159239",
-  appId: "1:914033159239:web:16e21a3018f00f46535562",
-};
-
-// Initialize firebase
-firebase.initializeApp(firebaseConfig);
-
 $("document").ready(function () {
   var typed = new Typed("#typing", {
     strings: ["Web Developer.", "UI/UX Designer.", "ML Enthusiast."],
@@ -19,48 +6,3 @@ $("document").ready(function () {
     loop: true,
   });
 });
-
-const menu = document.getElementsByClassName("menu")[0];
-const sections = document.getElementsByClassName("sections")[0];
-const socialMedia = document.getElementsByClassName("socialMedia")[0];
-const navbar = document.getElementsByClassName("navbar")[0];
-
-menu.addEventListener("click", () => {
-  sections.classList.toggle("active");
-  socialMedia.classList.toggle("active");
-  navbar.classList.toggle("active");
-});
-
-// reference your database
-var contactFormDB = firebase.database().ref("contactForm");
-
-document.getElementById("contactForm").addEventListener("submit", submitForm);
-
-function submitForm(e) {
-  e.preventDefault();
-
-  var name = getElementVal("name");
-  var mail = getElementVal("mail");
-  var phone = getElementVal("phone");
-  var msg = getElementVal("msg");
-
-  saveData(name, mail, phone, msg);
-
-  // reset form
-  document.getElementById("contactForm").reset();
-}
-
-const saveData = (name, mail, phone, msg) => {
-  var newContactForm = contactFormDB.push();
-
-  newContactForm.set({
-    name: name,
-    mail: mail,
-    phone: phone,
-    msg: msg,
-  });
-};
-
-const getElementVal = (id) => {
-  return document.getElementById(id).value;
-};
